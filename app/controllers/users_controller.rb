f94dbdf9926d732202fca4bc53a5a20b8312beee
Joe <JoeNoonan27@gmail.com>
2013-09-23 15:50:34 -0500

    Fixing Travis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
 	def new
 		@page = "New"
-		session[:sess] = "This is my session"
-		cookies[:cook] = "This is my cookie"
 	end
 
 	def index
 	  @page = "Index"
-		render 'new'
+		# render 'new'
 	end
 end
